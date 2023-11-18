c30087efa2e933a555effa79cc2d7a757eb0393d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-11-18 23:31:03 +0530

    added link post api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import Link from "./models/Link.js";
 const app = express();
 app.use(express.json());
 
@@ -17,11 +17,37 @@ connectMongoDB();
 
 //health api
 app.get("/health", (req, res) => {
-    res.json({
-      success: true,
-      message: "Hello Anand...🥳🥳",
-    });
+  res.json({
+    success: true,
+    message: "Hello Anand...🥳🥳",
   });
+});
+
+app.post("/link", async (req, res) => {
+  const { url, slug } = req.body;
+
+  const randomSlug = Math.random().toString(36).substring(2, 7);
+
+  const link = new Link({
+    url: url,
+    slug: slug || randomSlug,
+  });
+
+  try {
+    const savedLink = await link.save();
+
+    return res.json({
+      success: true,
+      data: savedLink,
+      message: "Link saved successfully",
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
