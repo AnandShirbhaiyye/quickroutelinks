f9a37292bf0b3f5190d8d8000c56c3655dd27916
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-11-21 00:34:19 +0530

    feat: create quick route links card design

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,48 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="app-title mt-3">Quick-Route-Links🔗</h1>
       <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h3 className="text-center mt-3">Link Generation</h3>
+        <div className="card shadow-lg mt-5 p-5">
+          <div className="text-center mb-4">
+            <h2>Quick-Route-Links✅</h2>
           </div>
-          <div className="col-md-6">
-            <h3 className="text-center mt-3">All Links</h3>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card shadow-sm p-3">
+                <form>
+                  <h4 className="text-center mt-3 mb-4">Link Generation🔗</h4>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter URL"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Slug (optional)"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Short URL"
+                    />
+                  </div>
+                  <button className="btn btn-dark w-100 mb-3" type="button">
+                    <b>Do Magic🪄</b>
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="todo-container shadow-sm p-3 mt-2">
+                <h4 className="text-center mt-2 mb-4">All Links📃</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
