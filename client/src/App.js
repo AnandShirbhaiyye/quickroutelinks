249b4395e60934d8c8bf39fa86195f1bd4d161dc
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-11-21 00:06:46 +0530

    feat: added bootstrap and create basic design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <h1 className="app-title">Quick-Route-Links🔗</h1>
-    </div>
+      <h1 className="app-title mt-3">Quick-Route-Links🔗</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h3 className="text-center mt-3">Link Generation</h3>
+          </div>
+          <div className="col-md-6">
+            <h3 className="text-center mt-3">All Links</h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
